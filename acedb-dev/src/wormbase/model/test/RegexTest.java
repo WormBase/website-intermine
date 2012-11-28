231ff9aa982df6656a2d97f6d0973560a0636340
JD Wong <jdmswong@wormbase.org>
2012-11-28 11:02:33 -0500

    minor changes

--- acedb-dev/src/wormbase/model/test/RegexTest.java
+++ acedb-dev/src/wormbase/model/test/RegexTest.java
@@ -2,6 +2,8 @@ package wormbase.model.test;
 
 import static org.junit.Assert.*;
 
+import java.util.regex.*;
+
 import org.junit.Test;
 
 public class RegexTest {
@@ -9,10 +11,28 @@ public class RegexTest {
 	@Test
 	public void test() {
 		
-		String testString = "?Gene   Evidence #Evidence"; // DELETE
-		//testString = "?Gene";
+		String modelHeader = "?Gene   Evidence #Evidence"; 
+		String firstLine = "?Gene?WBGene00000001?	?tag?Evidence?	?tag?CGC_data_submission?";
 		
-		assertTrue("String doesn't match",testString.matches("^\\?Gene.*"));
+//		String regex = "^\\?(Gene).*";
+//
+//		Pattern p = Pattern.compile(regex);
+//		Matcher m = p.matcher(testString);
+//		
+//		String[] strAry = testString.split("\\s+");
+//		
+//		boolean b = m.matches();
+		
+		String[] spaceTokens = firstLine.split("\\s+");
+		
+		String[] qtokens = spaceTokens[0].split("\\?",0);
+		
+		String type = qtokens[1];
+		String ID = qtokens[2];
+
+		
+		assertTrue("JD: incorrect type",type.matches("Gene"));
+		assertTrue("JD: incorrect ID", ID.matches("WBGene00000001"));
 		
 	}
 
--- acedb-dev/src/wormbase/model/test/TestClass.java
+++ acedb-dev/src/wormbase/model/test/TestClass.java
@@ -3,7 +3,10 @@
  */
 package wormbase.model.test;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
 
 import wormbase.model.parser.FileParser;
 import wormbase.model.parser.ModelParser;
@@ -20,9 +23,37 @@ public class TestClass {
 	 */
 	public static void main(String[] args) {
 		
-		String datafile = "test/test_datafiles/models.wrm";
+		final String datafile = "test/test_datafiles/models.wrm";
 		
-		ModelParser mp = new ModelParser(datafile);
+//		Scanner user_input = new Scanner( System.in );
+//		String line;
+//		while( (line = user_input.nextLine()) != null ){
+//			System.out.println(line.matches("^\\s*//.*"));
+//		}
+		
+		
+		try {
+			FileParser fp = new FileParser(datafile);
+			
+			String[] dataChunk;
+			while( (dataChunk = fp.getDataObj()) != null ){ 
+			//WMDebug.debug(dataChunk[0]);  // DEBUG
+			
+				String firstLine = dataChunk[0];
+				if( firstLine.matches("^\\s*//.*") ){
+					continue;
+				}
+				
+				if( firstLine.split("\\s+").length > 1 ){
+					System.out.println( firstLine.split("\\s+")[0] );
+				}
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 		
 		System.out.println("End program");
