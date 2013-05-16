52709c1c5a97bd9436d84d44657ab6e774b9184e
JD Wong <jdmswong@wormbase.org>
2013-05-15 20:38:25 -0400

    lots of test files and test cases

--- acedb-dev/test/junit/test/test.java
+++ acedb-dev/test/junit/test/test.java
@@ -1,18 +1,20 @@
 package junit.test;
 
 import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import org.junit.Test;
 
 public class test {
-
-	
-	
 	@Test
-	public void test() {
-		char c = 'c';
-		String cs = charAt(c);
-		
-		assertEquals(3, three[three.length-1]);
+	public void test() throws Exception {
+		String str = "Gene:ID1:ID2";
+//		System.out.println(String.valueOf(str.length())+";");
+		str = null;
+		System.out.println(String.valueOf(str.length())+";");
 	}
 
 }
